4fe20b4bf9bd2bb93be32061b25b69d49c43ca84
KILLALLSKYWALKER <firdausdaus98@ymail.com>
2020-07-29 08:02:27 +0800

    feat:add dummy data for testing

---
+++ seeder/plan.js
@@ -0,0 +1,118 @@
+module.exports = data = [
+    {
+        'model': 'Plan',
+        'documents': [
+            {
+                'provider': 'Harapan',
+                'price': 700 ,
+                'description':[
+                    {
+                        'info':'Percuma khidmat tunda'
+                    },
+                    {
+                        'info':'Percuma basuh kereta di cawangan panel'
+                    },
+                    {
+                        'info':'Servis sepanjang hayat'
+                    },
+                    {
+                        'info':'24 Jam tempoh kelulusan'
+                    }
+                ],
+                'categoryCover':[
+                    'tunda',
+                    'servis'
+                ]
+            },
+            {
+                'provider': 'Usaha Jaya',
+                'price': 600 ,
+                'description':[
+                    {
+                        'info':'Percuma khidmat servis kereta'
+                    },
+                    {
+                        'info':'Percuma windscrenn cover'
+                    },
+                    {
+                        'info':'Percuma 24-Jam bantuan tepi jalan'
+                    },
+                    {
+                        'info':'Percuma 24 Jam tempoh kelulusan'
+                    }
+                ],
+                'categoryCover':[
+                    'servis',
+                    'tunda',
+                    'gantian',
+                ]
+            },
+            {
+                'provider': 'Zurich',
+                'price': 900 ,
+                'description':[
+                    {
+                        'info':'Percuma khidmat tunda sebanyak RM 200'
+                    },
+                    {
+                        'info':'Percuma pemandu tambahan tanpa had'
+                    },
+                    {
+                        'info':'Sesuai kegunaan e-hailing'
+                    },
+                    {
+                        'info':'Percuma 24 Jam tempoh kelulusan'
+                    }
+                ],
+                categoryCover:[
+                    'e-hailing',
+                    'tunda',
+                ]
+            },
+            {
+                'provider': 'Asia Phenomena',
+                'price': 2000 ,
+                'description':[
+                    {
+                        'info':'Penukaran atau jumpstart bateri percuma'
+                    },
+                    {
+                        'info':'Penukaran tayar pancit'
+                    },
+                    {
+                        'info':'Penggantian kunci'
+                    },
+                    {
+                        'info':'Penggantian kerusi kanak kanak'
+                    }
+                ],
+                'categoryCover':[
+                    'servis',
+                    'gantian',
+                ]
+            },
+            {
+                'provider': 'Dagobah',
+                'price': 1000 ,
+                'description':[
+                    {
+                        'info':'Penukaran bateri secara percuma'
+                    },
+                    {
+                        'info':'Penukaran minyak hitam secara percuma'
+                    },
+                    {
+                        'info':'Pampasan kemalangan sebanyak RM 10000'
+                    },
+                    {
+                        'info':'Kerusi kanak kanak percuma'
+                    }
+                ],
+                'categoryCover':[
+                    'servis',
+                    'gantian',
+                ]
+            },
+        ]
+    }
+];
